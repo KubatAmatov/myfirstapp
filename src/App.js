3f78c2c0c9353ff20e2cb71cca6bb6da02a87e9a
LEGION 5 <amatov2006@gmail.com>
2021-12-06 17:47:13 +0600

    added new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import MyComponent from "./MyComponent";
 import SVG from "./SVG";
+import API from "./API";
 
 function App() {
   return (
     <div className="App">
-      <MyComponent /> <SVG />
+      <MyComponent /> <SVG /> <API />
     </div>
   );
 }
