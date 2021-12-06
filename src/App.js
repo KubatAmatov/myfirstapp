5b7ade6d8f5e25bb720a785e1e387d7d863f4cf8
LEGION 5 <amatov2006@gmail.com>
2021-12-06 17:40:52 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import MyComponent from "./MyComponent";
+import SVG from "./SVG";
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent /> <SVG />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,9 @@
+function MyComponent() {
+  return (
+    <div>
+      <h2>title</h2>
+      <p>lorem3</p>
+    </div>
+  );
+}
+export default MyComponent;
